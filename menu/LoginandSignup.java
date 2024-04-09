bc9302737c4baafd9998d1ca51adf1d20afb0da9
eLtieK <kennezversion@gmail.com>
2024-04-09 23:04:58 +0700

    this is fe

---
+++ menu/LoginandSignup.java
@@ -0,0 +1,21 @@
+
+package menu;
+
+/**
+ *
+ * @author User
+ */
+public class LoginandSignup {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Login LoginFrame = new Login();
+         LoginFrame.setVisible(true);
+         LoginFrame.pack();
+         LoginFrame.setLocationRelativeTo(null);
+         
+    }
+    
+}
